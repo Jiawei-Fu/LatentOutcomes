867529043ebe4120492bbf664bac90a2a6941b53
Jiawei-Fu <fujiaweisteven@hotmail.com>
2026-03-04 10:49:10 -0500

    s3

--- R/estlatent_methods.R
+++ R/estlatent_methods.R
@@ -1,9 +1,11 @@
+#' @method print estlatent
 #' @export
 print.estlatent <- function(x, ...) {
   cat("estlatent object. Use summary(x) to view the coefficient table.\n")
   invisible(x)
 }
 
+#' @method summary estlatent
 #' @export
 summary.estlatent <- function(object, ...) {
   out <- list(
@@ -17,6 +19,7 @@ summary.estlatent <- function(object, ...) {
   out
 }
 
+#' @method print summary.estlatent
 #' @export
 print.summary.estlatent <- function(x, ...) {
   cat("Call:\n")
